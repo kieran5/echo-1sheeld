c3488a814dbf49b9c1b954b06166571184858379
b4003579 <Frank.c.ratcliffe@gmail.com>
2018-03-09 15:52:18 +0000

    Implemented altSoftSerial
    figured out altsoftserial

--- echo-1sheeld.ino
+++ echo-1sheeld.ino
@@ -1,37 +1,44 @@
 #define CUSTOM_SETTINGS
-#define INCLUDE_VOICE_RECOGNIZER_SHIELD
 #define INCLUDE_TERMINAL_SHIELD
-#define INCLUDE_TEXT_TO_SPEECH_SHIELD
 #define INCLUDE_INTERNET_SHIELD
 
 #include <OneSheeld.h>
+#include <AltSoftSerial.h>
 
-String nickname = "";
-int score = 0;
+AltSoftSerial xbee;
+
+
+
+String nickname = "Tyrone";
+int score = 420;
+
 String scoreString = (String) score;
-static String jsonString1 = "{\"nickname\": ";
-static String jsonString2 = ", \"score\": ";
+static String jsonString1 = "{\"nickname\": \"";
+static String jsonString2 = "\", \"score\": ";
 static String jsonString3 = "}";
+String jsonObject = jsonString1 + nickname + jsonString2 + scoreString + jsonString3;
 
 HttpRequest scoreRequest("https://kwillis.eu/hiscores");
 
 void setup()
 {
   OneSheeld.begin();
+  xbee.begin(9600);
+  
   
   scoreRequest.setOnSuccess(&onSuccess);
   scoreRequest.setOnFailure(&onFailure);
   scoreRequest.addHeader("Content-Type", "application/json");
 
-  Terminal.println(&(jsonString1 + nickname + jsonString2 + scoreString + jsonString3)[0]);
-  scoreRequest.addRawData(&(jsonString1 + nickname + jsonString2 + scoreString + jsonString3)[0]);
+  Terminal.println(&(jsonObject)[0]);
+  scoreRequest.addRawData(&(jsonObject)[0]);
   
   Internet.performPost(scoreRequest);
 }
 
 void loop()
 {
-  
+  xbee.println("Hello John");
 }
 
 void onSuccess (HttpResponse &res)
