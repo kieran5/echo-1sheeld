687875bba8f14bc78b3e0c60acac38ae1d3bae08
Jake <b4026170@my.shu.ac.uk>
2018-03-11 23:11:12 +0000

    Added accurate turning using gyro

--- zumo-sketch/zumo-sketch.ino
+++ zumo-sketch/zumo-sketch.ino
@@ -1,8 +1,15 @@
 //includes
+
+//for zumo
 #include <ZumoMotors.h>
 #include <ZumoBuzzer.h>
 #include <ZumoMotors.h>
 #include <Pushbutton.h>
+
+//for gyro
+#include <L3G.h>
+#include <TurnSensor.h>
+#include <Utilities.h>
 
 #define LED 13 //for the Zumo LED
 
@@ -10,16 +17,14 @@ ZumoBuzzer buzzer;
 ZumoMotors motors;
 Pushbutton button(ZUMO_BUTTON); // pushbutton on pin 12
 
-
-//global variables
-
-//left and right motor speed, adjust here for known motor imbalance
-int lms = 100; int rms = 100;
+L3G gyro;
 
 int myId = -1;
 int reqId = -2;
+
 String req = "";
 String motor;
+
 char value;
 char reqCommand;
 
@@ -27,14 +32,17 @@ void setup()
 {
   //turn LED on and wait for button push before playing tune and handshake with processing
   digitalWrite(LED, HIGH);
+  
+  Serial.begin(9600);
+  turnSensorSetup();
+
+
+  
   button.waitForButton();
+  
   digitalWrite(LED, LOW);
   buzzer.play(">g32>>c32");
-  delay(1000);
 
-  Serial.begin(9600);
-
-  delay(200);
 
   Serial.println('I');
   
@@ -52,6 +60,8 @@ void setup()
 
 void loop()
 {
+  turn(-90);
+  delay(1000);
   if (Serial.available() > 0) {
     value = (char) Serial.read();
 
@@ -78,4 +88,33 @@ void loop()
       reqId = -2;
     }
   }
+}
+
+void turn(int target){
+
+    if(target > 0)
+        Serial.println("turning anti-clockwise");
+    else
+        Serial.println("turning clockwise");
+
+    turnSensorReset();
+    int32_t currHeading = Utilities::wrapAngle(turnSensorUpdate());
+    int32_t targetAngle = Utilities::wrapAngle(currHeading + target);
+    float error, speed;
+    bool done = false;
+    while(!done){
+
+        error = Utilities::wrapAngle(turnSensorUpdate() - targetAngle);
+        done = Utilities::inRange(fabs(error), (float) -1,(float) 1);
+
+        speed = 150;
+
+        if (error > 0) {
+            motors.setSpeeds(speed, -speed);
+        }
+        else {
+            motors.setSpeeds(-speed, speed);
+        }
+    }
+    motors.setSpeeds(0, 0);
 }
