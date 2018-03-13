60793cb8d44ac4eb086b0de39fe133af599c0f1e
b4003579 <Frank.c.ratcliffe@gmail.com>
2018-03-13 15:36:58 +0000

    Updated matrix functions

--- zumo-sketch/zumo-sketch.ino
+++ zumo-sketch/zumo-sketch.ino
@@ -1,25 +1,40 @@
 //includes
+
+//for zumo
 #include <ZumoMotors.h>
 #include <ZumoBuzzer.h>
 #include <ZumoMotors.h>
 #include <Pushbutton.h>
 
+
+//for reflectance sensors
+#include <QTRSensors.h>
+#include <ZumoReflectanceSensorArray.h>
+
+//for gyro
+#include <L3G.h>
+#include <TurnSensor.h>
+#include <Utilities.h>
+
+#define QTR_THRESHOLD 1000; //desired color threshold for QTR sensors. 
+#define NUM_SENSORS 6 //no. of sensors that the ZumoReflectanceSensorArray has
 #define LED 13 //for the Zumo LED
 
 ZumoBuzzer buzzer;
 ZumoMotors motors;
 Pushbutton button(ZUMO_BUTTON); // pushbutton on pin 12
+ZumoReflectanceSensorArray sensors(QTR_NO_EMITTER_PIN);
 
+unsigned int sensor_values[NUM_SENSORS]; //create an array which holds an int value for each one of our sensors to return a value to.
 
-//global variables
-
-//left and right motor speed, adjust here for known motor imbalance
-int lms = 100; int rms = 100;
+L3G gyro;
 
 int myId = -1;
 int reqId = -2;
+
 String req = "";
 String motor;
+
 char value;
 char reqCommand;
 
@@ -27,17 +42,20 @@ void setup()
 {
   //turn LED on and wait for button push before playing tune and handshake with processing
   digitalWrite(LED, HIGH);
-  button.waitForButton();
-  digitalWrite(LED, LOW);
-  buzzer.play(">g32>>c32");
-  delay(1000);
 
   Serial.begin(9600);
+  turnSensorSetup();
 
-  delay(200);
+
+
+  button.waitForButton();
+
+  digitalWrite(LED, LOW);
+  buzzer.play(">g32>>c32");
+
 
   Serial.println('I');
-  
+
   delay(200);
 
   byte b1 = Serial.read();
@@ -46,12 +64,14 @@ void setup()
   myId = b1 * 256 + b2;
 
   delay(100);
-  
+
   Serial.println(myId);
 }
 
 void loop()
 {
+  sensors.read(sensor_values);
+
   if (Serial.available() > 0) {
     value = (char) Serial.read();
 
@@ -62,15 +82,33 @@ void loop()
         reqId = idString.toInt();
         req = Serial.readString();
         reqCommand = req.charAt(0);
-       
+
     }
 
     if (reqId == myId) {
       //request was for us...
-      if(reqCommand == 'w') {
+      if (reqCommand == 'w') {
         motors.setSpeeds(100, 100);
         delay(2500);
-        motors.setSpeeds(0, 0);         
+        motors.setSpeeds(0, 0);
+      }
+      else if (reqCommand == 'a') {
+        turn(90);
+        motors.setSpeeds(100, 100);
+        delay(2500);
+        motors.setSpeeds(0, 0);
+
+      }
+      else if (reqCommand == 's') {
+        motors.setSpeeds(-100, -100);
+        delay(2500);
+        motors.setSpeeds(0, 0);
+      }
+      else if (reqCommand == 'd') {
+        turn(-90);
+        motors.setSpeeds(100, 100);
+        delay(2500);
+        motors.setSpeeds(0, 0);
       }
 
       // Reset request variables ready for next request
@@ -78,4 +116,33 @@ void loop()
       reqId = -2;
     }
   }
+}
+
+void turn(int target) {
+
+  if (target > 0)
+    Serial.println("turning anti-clockwise");
+  else
+    Serial.println("turning clockwise");
+
+  turnSensorReset();
+  int32_t currHeading = Utilities::wrapAngle(turnSensorUpdate());
+  int32_t targetAngle = Utilities::wrapAngle(currHeading + target);
+  float error, speed;
+  bool done = false;
+  while (!done) {
+
+    error = Utilities::wrapAngle(turnSensorUpdate() - targetAngle);
+    done = Utilities::inRange(fabs(error), (float) - 1, (float) 1);
+
+    speed = 150;
+
+    if (error > 0) {
+      motors.setSpeeds(speed, -speed);
+    }
+    else {
+      motors.setSpeeds(-speed, speed);
+    }
+  }
+  motors.setSpeeds(0, 0);
 }
