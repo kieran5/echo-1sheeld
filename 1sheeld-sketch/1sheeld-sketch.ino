681ddfd3451a8bc05036b355de7eb1774802c6da
kieran5 <kieranwillisx@gmail.com>
2018-03-16 13:22:45 +0000

    Moved working code over from voice branch.

--- 1sheeld-sketch/1sheeld-sketch.ino
+++ 1sheeld-sketch/1sheeld-sketch.ino
@@ -5,35 +5,34 @@
 #define INCLUDE_VOICE_RECOGNIZER_SHIELD
 #define INCLUDE_TEXT_TO_SPEECH_SHIELD
 
-#include <OneSheeld.h>
 #include <AltSoftSerial.h>
-#include <Player.h>
 #include <ArduinoSTL.h>
-#include <vector>
+#include <OneSheeld.h>
+#include <Player.h>
 
 using namespace std;
 
 //Set up SoftwareSerial for our xBee comms (AltSoftSerial using Rx Tx pins 8 and 9 on Arduino Uno board)
 AltSoftSerial xBee;
 
+vector<Player*> players;
+
 char value;
 
 int connectionCount = 0;
-
-vector<Player*> players;
 
 //Our player's name and their score
 String nickname = "";
 int score = 0;
 
 //A string to construct a JSON object containing users nickname and score, so we can make API request with data
-String jsonObject = "{\"nickname\": \"" + nickname + "\", \"score\": " + (String) score + "}";
+String jsonObject = "{\"nickname\": \"" + nickname + "\", \"score\": " + (String)score + "}";
 
 //Our API route for handling score data
 HttpRequest scoreRequest("https://kwillis.eu/hiscores");
 
 // Voice recognition
-bool gameStarted = false;
+bool voiceCommandActive = false;
 // Commands to be recognised by Zumos
 const char startGameCommand[] = "start game";
 const char forwardCommand[] = "forward";
@@ -44,374 +43,144 @@ const char rightCommand[] = "right";
 // Variable so the 1Sheeld knows who's turn it is next
 int playerToMoveNext = 1;
 
-// This stuff explains what is in the [z]axis of the matrix array. It is not needed by the program, but is laid out here for understanding
-// 0 for no, 1 for zumo,
-//int zumo?; //z[0]
-
-//0 for no, 1 for bomb
-//int bomb?: //z[1]
-
-//the playerID of the zumo or bomb
-//int playerID; //z[2]
-
-//the facing of a zumo, 1 is north/up, 2 is east/right, 3 is south/down, 4 is west/left
-//int orientation; //z[3]
-
-//does this zumo still have a bomb it can drop, 1 for yes, 0 for no.
-//int hasBomb; //z[4]
-
-//The current score of the zumo
-//int Score; //z[5]
-
-
-
-//This is actual program stuff
-
-//the 3dimensional array that holds the game state
-int matrix[4][4][6];
-
-int newX;
-int newY;
-
-//should be called after the zumo has physically moved. returns true if the move is safe, returns false if the zumo is dead.
-bool moveZumo(int playerID) {
-
-  //search the array for the correct zumo, by looping through the array a row at a time and checking if each cell contains a zumo with the correct user id
-  for (int x = 0; x < 4; x++)
-  {
-    for (int y = 0; y < 4; y++)
-    {
-      if (matrix[x][y][0] == 1 && matrix[x][y][2] == playerID)
-      {
-
-        //set the co-ordinates that the zuumo has moved to
-        switch (matrix[x][y][3])
-        {
-          case 1:
-            newY = y + 1;
-            newX = x;
-            break;
-          case 2:
-            newX = x + 1;
-            newY = y;
-            break;
-          case 3:
-            newY = y - 1;
-            newX = x;
-            break;
-          case 4:
-            newX = x + 1;
-            newY = y;
-            break;
-        }
-
-
-        if (ContainsBomb(newX, newY))
-        {
-          //take the id of the player who left the bomb
-          int id = matrix[newX][newY][2];
-
-          //remove the bomb
-          matrix[newX][newY][1] = 0;
-          matrix[newX][newY][2] = 0;
-
-          //search for their zumo in the array
-          for (int x = 0; x < 4; x++)
-          {
-            for (int y = 0; y < 4; y++)
-            {
-              if (matrix[x][y][0] == 1 && matrix[x][y][2] == id)
-
-                //Give that zumo a new bomb
-                matrix[x][y][4] = 1;
-              // increase its score
-              matrix[x][y][5] = matrix[x][y][5] + 5;
-
-              //the current player's zumo is dead. Post their score.
-              postScore(matrix[x][y][2], matrix[x][y][5]);
-
-              //if they have deployed their bomb, find it and remove it
-              if (matrix[x][y][4] == 0)
-              {
-                for (int x = 0; x < 4; x++)
-                {
-                  for (int y = 0; y < 4; y++)
-                  {
-                    if (matrix[x][y][1] == 1 && matrix[x][y][2] == playerID)
-                    {
-                      matrix[x][y][1] = 0;
-                      matrix[x][y][2] = 0;
-                    }
-                  }
-                }
-              }
-            }
-          }
-
-          //remove their zumo from the grid
-          matrix[x][y][0] = 0;
-          matrix[x][y][3] = 0;
-          matrix[x][y][4] = 0;
-          matrix[x][y][5] = 0;
-
-          return false;
-        }
-        //if you haven't hit a bomb
-        //move the representation of the zumo to the new square
-        //don't move the bomb!
-        matrix[newX][newY][0] = matrix[x][y][0];
-        matrix[newX][newY][2] = matrix[x][y][2];
-        matrix[newX][newY][3] = matrix[x][y][3];
-        matrix[newX][newY][4] = matrix[x][y][4];
-        matrix[newX][newY][5] = matrix[x][y][5] + 1;
-        //clear the previous square
-        //don't clear the bomb!
-        matrix[x][y][0] = 0;
-        matrix[x][y][3] = 0;
-        matrix[x][y][4] = 0;
-        matrix[x][y][5] = 0;
-        //don't clear the user ID if a bomb has been dropped
-        if (matrix[x][y][1] == 0)
-        {
-          matrix[x][y][2] = 0;
-        }
-
-        return true;
-      }
-    }
-  }
-}
-
-
-
-//returns playerID if the co-ordinates contain a bomb, and 0 if they do not
-bool ContainsBomb(int x, int y) {
-  if (matrix[x][y][1] = 1)
-  {
-    return true;
-  }
-  return false;
-}
-
-void setZumo(int playerID) {
-  matrix[1][playerID][0] = 1;
-  matrix[1][playerID][1] = 0;
-  matrix[1][playerID][2] = playerID - 1;
-  matrix[1][playerID][3] = 1;
-  matrix[1][playerID][4] = 1;
-  matrix[1][playerID][5] = 0;
-}
-
-//call this when a player signals they want to drop their bomb. Players should drop their bomb before they move
-void setBomb(int playerID, char dir) {
-  //search for the players zumo and drop a bomb on the same tile
-  for (int x = 0; x < 4; x++)
-  {
-    for (int y = 0; y < 4; y++)
-    {
-      if (matrix[x][y][0] == 1 && matrix[x][y][2] == playerID)
-      {
-
-      }
-    }
-  }
-}
-
-//posts a score. is called when a zumo dies, needs to be called for the last man standing, we need to figure out how to tell when the last man is standing.
-void postScore(int playerID, int playerScore)
-{
-  //needs sorting
-  //there needs to be an array that holds playerID's and nicknames which we can compare here
-  //then post player nickname and score to the website
-}
-
-//tells the zumo if you have turned, call this each time a 90 degree turn is made
-void setOrientation(int playerID, char turnVal) {
-  //Search for the correct zumo
-  for (int x = 0; x < 4; x++)
-  {
-    for (int y = 0; y < 4; y++)
-    {
-      if (matrix[x][y][0] == 1 && matrix[x][y][2] == playerID)
-      {
-
-        //for turning right
-        // advance round the compass clockwise 1 position
-        if (turnVal == 'r') {
-          if (matrix[x][y][3] == 4)
-          {
-            matrix[x][y][3] = 1;
-          } else {
-            matrix[x][y][3] = matrix[x][y][3] + 1;
-          }
-        }
-        //for turning left
-        // advance round the compass anticlockwise 1 position
-        if (turnVal == 'l') {
-          if (matrix[x][y][3] == 1)
-          {
-            matrix[x][y][3] = 4;
-          } else {
-            matrix[x][y][3] = matrix[x][y][3] - 1;
-          }
-        }
-
-      }
-    }
-  }
-}
+unsigned long lastConnectedTime = millis();
 
 void setup()
 {
-  //Set up serial on 115200 baud rate for our 1Sheeld+
-  OneSheeld.begin();
+    //Set up HTTP request for RESTful API calls
+    scoreRequest.setOnSuccess(&onSuccess);
+    scoreRequest.setOnFailure(&onFailure);
+    scoreRequest.addHeader("Content-Type", "application/json");
 
-  //Set up our xBee on 9600 baud rate (needs to be at a lower baud rate than our UART serial, as our UART is handling lock interrupts faster
-  xBee.begin(9600);
+    //Set up serial on 115200 baud rate for our 1Sheeld+
+    OneSheeld.begin();
 
-  //Set up HTTP request for RESTful API calls
-  scoreRequest.setOnSuccess(&onSuccess);
-  scoreRequest.setOnFailure(&onFailure);
-  scoreRequest.addHeader("Content-Type", "application/json");
+    //Set up our xBee on 9600 baud rate (needs to be at a lower baud rate than our UART serial, as our UART is handling lock interrupts faster
+    xBee.begin(9600);
+    xBee.println("Sheeld First Broadcast");
 
-  xBee.println("Sheeld First Broadcast");
-  
-  
-  while(!gameStarted) {
-      VoiceRecognition.start();
-      value = (char) xBee.read();
+    while (millis() - lastConnectedTime < 10000) {
+        if (xBee.available() > 0) {
 
-      if (VoiceRecognition.isNewCommandReceived()) {
-        if(strstr(VoiceRecognition.getLastCommand(), startGameCommand)) {
-          gameStarted = true;
-          break;           
+            value = (char)xBee.read();
+
+            switch (value) {
+            case 'I':
+                ++connectionCount;
+                xBee.println(connectionCount);
+
+                bool nickAssigned = false;
+
+                TextToSpeech.say("Say you name now.");
+                delay(2000); //so john can talk without affecting voice recognition
+                VoiceRecognition.start();
+                delay(5000); //so player has time to say there name after start()
+
+                while (!nickAssigned) {
+                    
+                    if (VoiceRecognition.isNewCommandReceived()) {
+                        String nickname = VoiceRecognition.getLastCommand();
+                        Player* player = new Player(connectionCount, nickname);
+                        players.push_back(player);
+                        nickAssigned = true;
+                        TextToSpeech.say("Successfully registered, thanks.");
+                        Terminal.println("true");
+                    } else {
+                        Terminal.println("false");
+                    }
+                }
+                lastConnectedTime = millis();
+                break;
+            }
         }
-      }
-      else if(value == 'I') {
-        ++connectionCount;
-        delay(100);
-        xBee.write(connectionCount / 256);
-        xBee.write(connectionCount % 256);
-        xBee.write(0x0A);
-
-        delay(1000);
-
-        TextToSpeech.say("Player " + String(connectionCount) + " say your name.");
-        
-        delay(2000);
-
-        bool nicknameAssigned = false;
-        while(!nicknameAssigned) {
-          VoiceRecognition.start();
-          if (VoiceRecognition.isNewCommandReceived()) {
-            
-            Terminal.println("New command received");
-            
-            String nickname = VoiceRecognition.getLastCommand();
-  
-            Terminal.println("yaname - " + nickname);
-  
-            Player* player = new Player(connectionCount, nickname);
-            players.push_back(player);
-  
-            int playerID = players[0]->getPlayerID();
-            String playerNickname = players[0]->getNickname();
-            TextToSpeech.say("Player " + String(playerID) + " is called " + playerNickname);
-            Terminal.println("Player " + String(playerID) + " is called " + playerNickname);
-
-            nicknameAssigned = true;
-            break;
-          }          
-        }
-        
-      }    
-  }
-
-  TextToSpeech.say("Game is now starting.");
-  
+    }
+    TextToSpeech.say(String(connectionCount) + " players connected, the game will start now.");
+    delay(4000);
 }
 
 void loop()
 {
-  // At least 2 players are required to play the game
-  //if(connectionCount > 1) {
-    
     VoiceRecognition.start();
     
     if(VoiceRecognition.isNewCommandReceived()) {
-      if(strstr(VoiceRecognition.getLastCommand(), forwardCommand) && gameStarted) {
+      if(strstr(VoiceRecognition.getLastCommand(), forwardCommand)) {
         moveZumo(playerToMoveNext, 'w');
+        delay(3000);
         nextPlayersTurn();
         
       }
-      else if(strstr(VoiceRecognition.getLastCommand(), leftCommand) && gameStarted) {
+      else if(strstr(VoiceRecognition.getLastCommand(), leftCommand)) {
         moveZumo(playerToMoveNext, 'a');
+        delay(3000);
         nextPlayersTurn();
                         
       }
-      else if(strstr(VoiceRecognition.getLastCommand(), backwardCommand) && gameStarted) {
+      else if(strstr(VoiceRecognition.getLastCommand(), backwardCommand)) {
         moveZumo(playerToMoveNext, 's');
+        delay(3000);
         nextPlayersTurn();
                 
       }
-      else if(strstr(VoiceRecognition.getLastCommand(), rightCommand) && gameStarted) {
+      else if(strstr(VoiceRecognition.getLastCommand(), rightCommand)) {
         moveZumo(playerToMoveNext, 'd');
+        delay(3000);
         nextPlayersTurn();
             
       }
-  
   
       // Update Zumo's current location (cell number)
   
       // Check if new cell contains a bomb
             
+
+      return;
     
     }  
-  //}
 }
 
 void nextPlayersTurn() {
   if(connectionCount == playerToMoveNext) {
+    TextToSpeech.say("Player 1 Turn");
+    delay(1000);
     playerToMoveNext = 1;
   } else {
+    TextToSpeech.say("Next Players Turn");
+    delay(1000);
     playerToMoveNext++;
-  }
-
-  TextToSpeech.say("Next Players Turn");
-  
+  }  
 }
 
-void moveZumo(int connectionID, char dir) {
-
-  String toSend = String(connectionID) + ":" + dir;
-  
-  xBee.println('M');
-  xBee.println(toSend);
-
-}
-
-void submitScore() {
-  //Print JSON object to terminal for debugging
-  Terminal.println("Attempting to post:");
-  Terminal.println(&(jsonObject)[0]);
-
-  //Add data to our HTTP request and post to our API
-  scoreRequest.addRawData(&(jsonObject)[0]);
-  Internet.performPost(scoreRequest);
-}
-
-void onFailure (HttpResponse &res)
+void moveZumo(int connectionID, char dir)
 {
-  //Do below on an unsuccessful HTTP request
-  Terminal.println("Request failed");
-  Terminal.println(res.getStatusCode());
-  Terminal.println(res.getTotalBytesCount());
+
+    String toSend = String(connectionID) + ":" + dir;
+    xBee.println('M');
+    xBee.println(toSend);
 }
 
-void onSuccess (HttpResponse &res)
+void submitScore()
 {
-  //Do below on a successful HTTP request
-  Terminal.println("Request succeeded");
-  Terminal.println(res.getStatusCode());
-  Terminal.println(res.getTotalBytesCount());
+    //Print JSON object to terminal for debugging
+    Terminal.println("Attempting to post:");
+    Terminal.println(&(jsonObject)[0]);
+
+    //Add data to our HTTP request and post to our API
+    scoreRequest.addRawData(&(jsonObject)[0]);
+    Internet.performPost(scoreRequest);
+}
+
+void onFailure(HttpResponse& res)
+{
+    //Do below on an unsuccessful HTTP request
+    Terminal.println("Request failed");
+    Terminal.println(res.getStatusCode());
+    Terminal.println(res.getTotalBytesCount());
+}
+
+void onSuccess(HttpResponse& res)
+{
+    //Do below on a successful HTTP request
+    Terminal.println("Request succeeded");
+    Terminal.println(res.getStatusCode());
+    Terminal.println(res.getTotalBytesCount());
 }
--- zumo-sketch/zumo-sketch.ino
+++ zumo-sketch/zumo-sketch.ino
@@ -1,11 +1,9 @@
 //includes
 
 //for zumo
-#include <ZumoMotors.h>
+#include <Pushbutton.h>
 #include <ZumoBuzzer.h>
 #include <ZumoMotors.h>
-#include <Pushbutton.h>
-
 
 //for reflectance sensors
 #include <QTRSensors.h>
@@ -16,7 +14,7 @@
 #include <TurnSensor.h>
 #include <Utilities.h>
 
-#define QTR_THRESHOLD 1000; //desired color threshold for QTR sensors. 
+#define QTR_THRESHOLD 1000; //desired color threshold for QTR sensors.
 #define NUM_SENSORS 6 //no. of sensors that the ZumoReflectanceSensorArray has
 #define LED 13 //for the Zumo LED
 
@@ -40,155 +38,133 @@ char reqCommand;
 
 void setup()
 {
-  //turn LED on and wait for button push before playing tune and handshake with processing
-  digitalWrite(LED, HIGH);
+    //turn LED on and wait for button push before playing tune and handshake with processing
+    digitalWrite(LED, HIGH);
 
-  Serial.begin(9600);
-  turnSensorSetup();
+    Serial.begin(9600);
+    turnSensorSetup();
 
+    button.waitForButton();
 
+    digitalWrite(LED, LOW);
+    buzzer.play(">g32>>c32");
 
-  button.waitForButton();
+    Serial.println('I');
 
-  digitalWrite(LED, LOW);
-  buzzer.play(">g32>>c32");
+    myId = Serial.parseInt();
 
-
-  Serial.println('I');
-
-  delay(200);
-
-  byte b1 = Serial.read();
-  byte b2 = Serial.read();
-
-  myId = b1 * 256 + b2;
-
-  delay(100);
-
-  Serial.println(myId);
+    Serial.println(myId);
 }
 
 void loop()
 {
-  sensors.read(sensor_values);
+    sensors.read(sensor_values);
 
-  if (Serial.available() > 0) {
-    value = (char) Serial.read();
+    if (Serial.available() > 0) {
+        value = (char)Serial.read();
 
-    switch (value)
-    {
-      case 'M':
-        String idString = Serial.readStringUntil(':');
-        reqId = idString.toInt();
-        req = Serial.readString();
-        reqCommand = req.charAt(0);
+        switch (value) {
+        case 'M':
+            String idString = Serial.readStringUntil(':');
+            reqId = idString.toInt();
+            req = Serial.readString();
+            reqCommand = req.charAt(0);
+        }
 
+        if (reqId == myId) {
+            //request was for us...
+            if (reqCommand == 'w') {
+                advance();
+            } else if (reqCommand == 'a') {
+                turn(45);
+                delay(500);
+                turn(45);
+                advance();
+            } else if (reqCommand == 's') {
+                turn(45);
+                delay(500);
+                turn(45);
+                delay(500);
+                turn(45);
+                delay(500);
+                turn(45);
+                advance();
+            } else if (reqCommand == 'd') {
+                turn(-45);
+                delay(500);
+                turn(-45);
+                advance();
+            }
+
+            // Reset request variables ready for next request
+            req = "";
+            reqId = -2;
+        }
     }
-
-    if (reqId == myId) {
-      //request was for us...
-      if (reqCommand == 'w') {
-        motors.setSpeeds(100, 100);
-        delay(2500);
-        motors.setSpeeds(0, 0);
-      }
-      else if (reqCommand == 'a') {
-        turn(90);
-        motors.setSpeeds(100, 100);
-        delay(2500);
-        motors.setSpeeds(0, 0);
-
-      }
-      else if (reqCommand == 's') {
-        motors.setSpeeds(-100, -100);
-        delay(2500);
-        motors.setSpeeds(0, 0);
-      }
-      else if (reqCommand == 'd') {
-        turn(-90);
-        motors.setSpeeds(100, 100);
-        delay(2500);
-        motors.setSpeeds(0, 0);
-      }
-      else if(reqCommand == 'a') {
-        turn(90);
-        motors.setSpeeds(100, 100);
-        delay(2500);
-        motors.setSpeeds(0, 0);
-        
-      }
-      else if(reqCommand == 's') {
-        motors.setSpeeds(-100, -100);
-        delay(2500);
-        motors.setSpeeds(0, 0);
-      }
-      else if(reqCommand == 'd') {
-        turn(-90);
-        motors.setSpeeds(100, 100);
-        delay(2500);
-        motors.setSpeeds(0, 0);        
-      }
-
-      // Reset request variables ready for next request
-      req = "";
-      reqId = -2;
-    }
-  }
 }
 
-void turn(int target) {
+void turn(int target)
+{
 
-  if (target > 0)
-    Serial.println("turning anti-clockwise");
-  else
-    Serial.println("turning clockwise");
+    if (target > 0)
+        Serial.println("turning anti-clockwise");
+    else
+        Serial.println("turning clockwise");
 
-  turnSensorReset();
-  int32_t currHeading = Utilities::wrapAngle(turnSensorUpdate());
-  int32_t targetAngle = Utilities::wrapAngle(currHeading + target);
-  float error, speed;
-  bool done = false;
-  while (!done) {
+    turnSensorReset();
+    int32_t currHeading = Utilities::wrapAngle(turnSensorUpdate());
+    int32_t targetAngle = Utilities::wrapAngle(currHeading + target);
+    float error, speed;
+    bool done = false;
+    while (!done) {
 
-    error = Utilities::wrapAngle(turnSensorUpdate() - targetAngle);
-    done = Utilities::inRange(fabs(error), (float) - 1, (float) 1);
+        error = Utilities::wrapAngle(turnSensorUpdate() - targetAngle);
+        done = Utilities::inRange(fabs(error), (float)-1, (float)1);
 
-    speed = 150;
+        speed = 150;
 
-    if (error > 0) {
-      motors.setSpeeds(speed, -speed);
+        if (error > 0) {
+            motors.setSpeeds(speed, -speed);
+        } else {
+            motors.setSpeeds(-speed, speed);
+        }
     }
-    else {
-      motors.setSpeeds(-speed, speed);
-    }
-  }
-  motors.setSpeeds(0, 0);
+    motors.setSpeeds(0, 0);
 }
 
-void advance() {
-  motors.setSpeeds(150, 150);
+void advance()
+{
+    motors.setSpeeds(100, 100);
 
-  bool hasCrossedLine = false;
-  bool isAtEnd = false;
+    bool hasCrossedLine = false;
+    bool isAtEnd = false;
 
+    while (!isAtEnd) {
 
-  while (!isAtEnd) {
-    
-    while (!hasCrossedLine) {
-      if (isOverLine(sensor_values[2]) && isOverLine(sensor_values[3])) {
-        delay(1000);
-        hasCrossedLine = true;
-      }
+        while (!hasCrossedLine) {
+
+            sensors.read(sensor_values);
+
+            if (isOverLine(sensor_values[2]) && isOverLine(sensor_values[3])) {
+                delay(1000);
+                hasCrossedLine = true;
+            }
+        }
+
+        sensors.read(sensor_values);
+
+        if (isOverLine(sensor_values[2]) && isOverLine(sensor_values[3])) {
+            isAtEnd = true;
+        }
     }
-    
-    if (isOverLine(sensor_values[2]) && isOverLine(sensor_values[3])) {
-      isAtEnd = true;
-    }   
-  }
-  motors.setSpeeds(0, 0);
+    motors.setSpeeds(0, 0);
+    delay(250);
+    motors.setSpeeds(-100, -100);
+    delay(350);
+    motors.setSpeeds(0, 0);
 }
 
-bool isOverLine(int sensorPin) {
-  return sensorPin > QTR_THRESHOLD;
+bool isOverLine(int sensorPin)
+{
+    return sensorPin > QTR_THRESHOLD;
 }
-
